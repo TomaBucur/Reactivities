583aaab38e76792225a972ca9404ecb2dc3011d9
TomaBucur <102102780+TomaBucur@users.noreply.github.com>
2022-12-23 16:15:54 +0200

    CRUD in React finished

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/layout/models/activity';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
@@ -7,20 +7,38 @@ import ActivityList from './ActivityList';
 
 interface Props{
     activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id:string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id:string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityDashboard({activities}: Props) {
+export default function ActivityDashboard({activities, selectedActivity, 
+    selectActivity,cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities}/>
+                <ActivityList 
+                activities={activities} 
+                selectActivity={selectActivity}
+                deleteActivity={deleteActivity}
+                />
             </Grid.Column>
             <Grid.Column width='6'>
                 {/* it allows the component ActivityDetails to be used
                   only if it has something at index 0 */}
-                {activities[0] &&
-                <ActivityDetails activity={activities[0]} />} 
-                <ActivityForm />
+                {selectedActivity && !editMode &&
+                <ActivityDetails 
+                activity={selectedActivity}  
+                cancelSelectActivity={cancelSelectActivity}
+                openForm={openForm}
+                />} 
+                {editMode &&
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
             </Grid.Column>
         </Grid>
     )
