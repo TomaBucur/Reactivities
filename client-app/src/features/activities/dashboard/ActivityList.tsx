9b7835f77a8a7ca58e9ec276081ba091156fa4c0
TomaBucur <102102780+TomaBucur@users.noreply.github.com>
2022-12-29 14:43:40 +0200

    activities page bug fixed, react-calendar bug fixed

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -4,17 +4,13 @@ import { Header } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import ActivityListItem from './ActivityListItem';
 
-
-
 export default observer(function ActivityList () {
     const{activityStore} = useStore();
     const{groupedActivities} = activityStore;
 
-
-
     return (
         <>
-            {groupedActivities.map(([group, activities]) => {
+            {groupedActivities.map(([group, activities]) => (
                 <Fragment key={group}>
                     <Header sub color='teal'>
                         {group}
@@ -23,7 +19,7 @@ export default observer(function ActivityList () {
                         <ActivityListItem key={activity.id} activity={activity} />
                     ))}
                 </Fragment>
-            })}
+            ))}
         </>
             
     )
