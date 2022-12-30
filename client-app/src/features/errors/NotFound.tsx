1f62e9edf28d1aa65ce5f24e6018d9633c367459
TomaBucur <102102780+TomaBucur@users.noreply.github.com>
2022-12-30 18:04:12 +0200

    Finished error handling

---
+++ client-app/src/features/errors/NotFound.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button, Header, Icon, Segment } from "semantic-ui-react";
+
+export default function NotFound() {
+    return(
+        <Segment placeholder>
+            <Header icon>
+                <Icon name="search"/>
+                Oops - we've looked everywhere and don't find what you were looking for!
+            </Header>
+            <Segment.Inline>
+                <Button as={Link} to='/activities'>
+                    Return to activity page
+                </Button>
+            </Segment.Inline>
+
+
+        </Segment>
+
+    )
+}
