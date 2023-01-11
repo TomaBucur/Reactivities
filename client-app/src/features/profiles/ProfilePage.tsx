ad516a7819f77cdbb1b0f31aa5fbd39379fd03db
TomaBucur <102102780+TomaBucur@users.noreply.github.com>
2023-01-11 20:34:32 +0200

    ProfileContent created/ProfilePage updated with menu

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,11 +1,13 @@
 import { Grid } from "semantic-ui-react";
+import ProfileContent from "./ProfileContent";
 import ProfileHeader from "./ProfileHeader";
 
 export default function ProfilePage() {
     return (
         <Grid>
             <Grid.Column width={16}>
-                <ProfileHeader/>
+                <ProfileHeader />
+                <ProfileContent />
             </Grid.Column>
         </Grid>
     )
