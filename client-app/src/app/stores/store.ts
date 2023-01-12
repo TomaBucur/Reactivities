2d13b00cd8c558446c59cf29588b497a2d6741b5
TomaBucur <102102780+TomaBucur@users.noreply.github.com>
2023-01-12 11:45:09 +0200

    commentStore added

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,5 +1,6 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
+import CommentStore from "./commentStore";
 import CommonStore from "./commonStore";
 import modalStore from "./modalStore";
 import ProfileStore from "./profileStore";
@@ -11,6 +12,7 @@ interface Store {
     userStore: UserStore;
     modalStore: modalStore;
     profileStore: ProfileStore;
+    commentStore: CommentStore;
 }
 
 export const store: Store = {
@@ -19,6 +21,7 @@ export const store: Store = {
     userStore: new UserStore(),
     modalStore: new modalStore(),
     profileStore: new ProfileStore(),
+    commentStore: new CommentStore(),
 }
 
 export const StoreContext = createContext(store);
